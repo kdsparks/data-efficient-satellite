48dfe266e766f3db20e1e81f7ce01d52e302e7c7
Cadie Sparks <cadiesparks@gmail.com>
2026-09-14 17:52:19 -0700

    Reformat and give more descriptive names

--- data-tx/data-tx.ino
+++ data-tx/data-tx.ino
@@ -25,6 +25,16 @@
   dramatically.
  ****************************************************
  SparkFun BMI270 Arduino Library: Example01_BasicReadingsI2C
+ ****************************************************
+ Read the temperature pixels from the MLX90640 IR array
+  By: Nathan Seidle
+  SparkFun Electronics
+  Date: May 22nd, 2018
+  License: MIT. See license file for more information but you can
+  basically do whatever you want with this code.
+
+  This example initializes the MLX90640 and outputs the 768 temperature values
+  from the 768 pixels.
 */
 
 #include <Wire.h>
@@ -34,14 +44,7 @@
 #include "MLX90640_API.h"
 #include "MLX90640_I2C_Driver.h"
 
-
-const byte MLX90640_address = 0x33; //Default 7-bit unshifted address of the MLX90640
-
-#define TA_SHIFT 8 //Default shift for MLX90640 in open air
-
-const int DATA_SIZE = 32 * 24; //variable for number of pixels
-
-static float mlx90640To[768]; 
+#define TA_SHIFT 8  // Default shift for MLX90640 in open air
 
 HUSKYLENS huskylens;
 SFE_UBLOX_GNSS myGNSS;
@@ -50,6 +53,9 @@ paramsMLX90640 mlx90640;
 
 long lastTime = 0;                           // Simple local timer. Limits amount of I2C traffic to u-blox module
 uint8_t i2cAddressImu = BMI2_I2C_PRIM_ADDR;  // 0x68
+const byte MLX90640_address = 0x33;          // Default 7-bit unshifted address of the MLX90640
+const int DATA_SIZE_IR = 32 * 24;            // Variable for number of pixels
+static float mlx90640To[768];
 
 void printResultHusky(HUSKYLENSResult result);
 void printPositionGNSS();
@@ -70,8 +76,7 @@ void setup() {
   // Set up GNSS
   Serial.println("Setting up GNSS...");
   //myGNSS.enableDebugging(); // Uncomment this line to enable helpful debug messages on Serial
-  while (!myGNSS.begin(Wire1))
-  {
+  while (!myGNSS.begin(Wire1)) {
     Serial.println(F("u-blox GNSS not detected at default I2C address. Please check wiring."));
     delay(3000);
   }
@@ -86,20 +91,19 @@ void setup() {
   {
     // Not connected, inform user
     Serial.println("Error: BMI270 not connected, check wiring and I2C address!");
-    // Wait a bit to see if connection is established
     delay(3000);
   }
   Serial.println("BMI270 connected!");
 
   //Set up IR Array
-  if (isConnected() == false)
-  {
-    Serial.println("MLX90640 not detected at default I2C address. Please check wiring. Freezing.");
-    while (1);
+  Serial.println("Setting up IR Array...");
+  while (!isConnectedMLX()) {
+    Serial.println("MLX90640 not detected at default I2C address. Please check wiring.");
+    delay(3000);
   }
   Serial.println("MLX90640 online!");
 
-  //Get device parameters - We only have to do this once
+  // Get device parameters for IR Array - We only have to do this once
   int status;
   uint16_t eeMLX90640[832];
   status = MLX90640_DumpEE(MLX90640_address, eeMLX90640);
@@ -109,8 +113,6 @@ void setup() {
   status = MLX90640_ExtractParameters(eeMLX90640, &mlx90640);
   if (status != 0)
     Serial.println("Parameter extraction failed");
-
-
 }
 
 void loop() {
@@ -133,13 +135,12 @@ void loop() {
   // the sensor data, otherwise it will never update
   imu.getSensorData();
 
-  //Get Temps from IR Array
-  for (byte x = 0 ; x < 2 ; x++) //Read both subpages
+  // Get Temps from IR Array
+  for (byte x = 0; x < 2; x++)  // Read both subpages
   {
     uint16_t mlx90640Frame[834];
     int status = MLX90640_GetFrameData(MLX90640_address, mlx90640Frame);
-    if (status < 0)
-    {
+    if (status < 0) {
       Serial.print("GetFrame Error: ");
       Serial.println(status);
     }
@@ -147,7 +148,7 @@ void loop() {
     float vdd = MLX90640_GetVdd(mlx90640Frame, &mlx90640);
     float Ta = MLX90640_GetTa(mlx90640Frame, &mlx90640);
 
-    float tr = Ta - TA_SHIFT; //Reflected temperature based on the sensor ambient temperature
+    float tr = Ta - TA_SHIFT;  //Reflected temperature based on the sensor ambient temperature
     float emissivity = 0.95;
 
     MLX90640_CalculateTo(mlx90640Frame, &mlx90640, emissivity, tr, mlx90640To);
@@ -155,17 +156,16 @@ void loop() {
 
   int num_hot_pixels = 0;
 
-  for (int x = 0 ; x < DATA_SIZE ; x++)
-  {
-    float pixel_temp = mlx90640To[x]; //Reads temps of all 768 pixels
-    if (pixel_temp > 35) //If the temp of a pixel is greater than 35C...
-      num_hot_pixels++; //...add it to the count of hot pixels
+  for (int x = 0; x < DATA_SIZE_IR; x++) {
+    float pixel_temp = mlx90640To[x];  //Reads temps of all 768 pixels
+    if (pixel_temp > 35)               //If the temp of a pixel is greater than 35C...
+      num_hot_pixels++;                //...add it to the count of hot pixels
   }
-  if (num_hot_pixels > 25) //If the count of hot pixels exceeds 25...
-    Serial.println("hot!!"); //declare it hot
-    
-// communicate with the husky to confirm/deny a fire
-  Serial.println(num_hot_pixels); 
+  if (num_hot_pixels > 25)    //If the count of hot pixels exceeds 25...
+    Serial.println("hot!!");  //declare it hot
+
+  // communicate with the husky to confirm/deny a fire
+  Serial.println(num_hot_pixels);
   delay(500);
 }
 
@@ -213,10 +213,9 @@ void printPositionGNSS() {
 }
 
 //Returns true if the MLX90640 is detected on the I2C bus
-boolean isConnected()
-{
+boolean isConnectedMLX() {
   Wire1.beginTransmission((uint8_t)MLX90640_address);
   if (Wire1.endTransmission() != 0)
-    return (false); //Sensor did not ACK
+    return (false);  //Sensor did not ACK
   return (true);
 }
