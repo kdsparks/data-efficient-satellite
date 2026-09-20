2e66d9fedec5f6fe47434644f1b1a639b798d633
Cadie Sparks <cadiesparks@gmail.com>
2026-09-20 16:31:55 -0700

    Set tx pin for RP2040 in RF tester file

--- rf/RadioHead/examples/ask/ask_transmitter/ask_transmitter.ino
+++ rf/RadioHead/examples/ask/ask_transmitter/ask_transmitter.ino
@@ -10,7 +10,7 @@
 #include <SPI.h> // Not actually used but needed to compile
 #endif
 
-RH_ASK driver;
+RH_ASK driver(2000, 17, 16); // set tx pin to 16
 // RH_ASK driver(2000, 4, 5, 0); // ESP8266 or ESP32: do not use pin 11 or 2
 // RH_ASK driver(2000, 3, 4, 0); // ATTiny, RX on D3 (pin 2 on attiny85) TX on D4 (pin 3 on attiny85), 
 // RH_ASK driver(2000, PD14, PD13, 0); STM32F4 Discovery: see tx and rx on Orange and Red LEDS
